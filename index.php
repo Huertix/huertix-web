17990d652415af582640aaae761f79eebffc3bd2
David Huerta <dhuerta23@gmail.com>
2015-08-25 10:11:20 +0200

    Under Construction

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 
-Web Under Construction.
+echo '<head>
+	<meta charset="UTF-8">
+	<title>Huertix Web</title>
+
+	<link href="style.css" rel="stylesheet" type="text/css">
+
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+
+
+	</head>';
+
+
+
+	echo '<a href="" onclick="goBack()"><h1>Web Under Construction<br>Temporally Disabled</h1></a>';
+
+	echo '<script type="text/javascript">
+		function goBack(){			
+			window.history.back();
+					 	
+		}
+	</script>';
+
+
 
 ?>
